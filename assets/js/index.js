6118321949e2f6c303adf8d8fd6ea98b431e0791
Gabriel de Freitas Laminas <laminas533@gmail.com>
2021-12-13 18:29:33 -0300

    add some js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
-//Selecionando os botões
 const menuOpen = document.querySelector('.icon--open');
 const menuClose = document.querySelector('.icon--close');
 const boxIcon = document.querySelector('.nav__icons');
@@ -8,4 +7,16 @@ boxIcon.addEventListener('click', () =>{
    menuOpen.classList.toggle('activated');
    menuClose.classList.toggle('activated');
    boxNavList.classList.toggle('activated');
+   document.body.classList.toggle('activated');
 });
+
+window.addEventListener('resize', function(e){
+   let widthWindow = document.body.clientWidth;
+   
+   if(widthWindow > 768){
+      boxNavList.classList.remove('activated');
+      menuOpen.classList.remove('activated');
+      menuClose.classList.remove('activated');
+      document.body.classList.remove('activated');
+   }
+})
